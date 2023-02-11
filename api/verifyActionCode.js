7ed9627c18bd6608cae9337f76d1dd29b3347a60
HughesSeanKyle <khughessean@yahoo.com>
2023-02-11 15:31:45 +0200

    Remove whitelist ofr testing

--- api/verifyActionCode.js
+++ api/verifyActionCode.js
@@ -7,16 +7,16 @@ export default async function verifyActionCode(req, res) {
 	try {
 		const { email, code } = req.body;
 
-		const whitelist = [
-			process.env.WHITELISTED_DOMAIN_ONE,
-			process.env.WHITELISTED_DOMAIN_TWO,
-			process.env.WHITELISTED_DOMAIN_THREE,
-		];
-		const origin = req.headers.origin;
+		// const whitelist = [
+		// 	process.env.WHITELISTED_DOMAIN_ONE,
+		// 	process.env.WHITELISTED_DOMAIN_TWO,
+		// 	process.env.WHITELISTED_DOMAIN_THREE,
+		// ];
+		// const origin = req.headers.origin;
 
-		if (!whitelist.includes(origin)) {
-			return res.status(403).json({ data: null, error: 'Forbidden' });
-		}
+		// if (!whitelist.includes(origin)) {
+		// 	return res.status(403).json({ data: null, error: 'Forbidden' });
+		// }
 
 		const response = await verifyCode(email, code);
 
